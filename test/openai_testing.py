396c0c21ae6ccde60024a5978a0856c84294f0e0
tungvh04 <116733524+tungvh04@users.noreply.github.com>
2025-01-02 18:38:22 +0700

    Add unit test for openai testing

---
+++ test/openai_testing.py
@@ -0,0 +1,33 @@
+import requests
+
+username = 'username'
+password = 'password'
+email = 'email'
+host = 'localhost'
+port = 5002
+
+signup_route = f'http://{host}:{port}/signup/{username}/{email}/{password}'
+signup_response = requests.post(signup_route)
+
+login_route = f'http://{host}:{port}/login/{username}/{password}'
+login_response = requests.post(login_route)
+user_id = login_response.json()['user_id']
+
+conversation_create_route = f'http://{host}:{port}/chatbot/create/{user_id}'
+conversation_create_response = requests.post(conversation_create_route)
+conversation_id = conversation_create_response.text
+
+message = 'Hello, who are you?'
+print(f'User message: {message}')
+conversation_send_message_route = f'http://{host}:{port}/chatbot/{conversation_id}/send_message/{message}'
+conversation_send_message_response = requests.post(conversation_send_message_route)
+assistant_response = conversation_send_message_response.json()['response']
+print(f'Assistant response: {assistant_response}')
+
+conversation_content_route = f'http://{host}:{port}/chatbot/{conversation_id}/content'
+conversation_content_response = requests.get(conversation_content_route)
+
+
+conversation_content = conversation_content_response.json()
+print(f'Conversation content: {conversation_content}')
+
